a38d9a700e0fe959f952f0ca921b27cc1f6dbfbf
Prashant <prashantsingh830@gmail.com>
2025-01-11 18:16:31 +0530

    updated secret

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use((req, res, next) => {
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: process.env.FE_HOST,
   })
 );
 app.use(cookieParser());
@@ -48,6 +48,6 @@ app.use("/api/likes", likeRoutes);
 app.use("/api/stories", storyRoutes);
 app.use("/api/relationships", relationshipRoutes);
 
-app.listen(8800, () => {
-  console.log("Server has started on port 8800");
+app.listen(8900, () => {
+  console.log("Server has started on port 8900");
 });
